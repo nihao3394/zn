33a9ab60d266e22b547ad1b7951a1b47633b3e4c
nihao3394 <209498022+nihao3394@users.noreply.github.com>
2026-07-25 23:42:26 +0800

    3.9.32 修改部分细节

--- about/config.js
+++
@@ -1,3 +0,0 @@
-window.MY_APP_CONFIG = {
-    apiBaseUrl: 'https://ps.findingstar.top/api/counter?action=hit&page=about'
-};
--- config.js
+++
@@ -1,4 +0,0 @@
-// index.html_script_fetch_
-window.MY_APP_CONFIG = {
-    apiBaseUrl: 'https://ps.findingstar.top/api/counter?action=hit&page=home'
-};
